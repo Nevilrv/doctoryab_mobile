4bf614354ca840aa32ded3f20875a8b72d541d76
nevilrv <nevilrv@gmail.com>
2023-10-04 13:22:53 +0530

    initial commit 1

---
+++ lib/app/theme/AppImages.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static const home = "assets/svg/Vector.svg";
+  static const message = "assets/svg/Chat_Dots.svg";
+  static const heart = "assets/svg/heart.svg";
+  static const enquiry = "assets/svg/News.svg";
+  static const profile = "assets/svg/profile.svg";
+  static const menu = "assets/svg/menu.svg";
+  static const bell = "assets/png/bell.png";
+  static const filter = "assets/png/filter.png";
+}
